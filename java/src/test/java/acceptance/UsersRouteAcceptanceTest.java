f7e31fd7df6824a23b691307fdbeb77ed64c3886
Daniele Megna <megna.dany@gmail.com>
2023-07-06 16:38:07 +0200

    Improve user register acceptance test
    parse json body and assert on returned fields

--- java/src/test/java/acceptance/UsersRouteAcceptanceTest.java
+++ java/src/test/java/acceptance/UsersRouteAcceptanceTest.java
@@ -1,6 +1,7 @@
 package acceptance;
 
-import org.junit.jupiter.api.Disabled;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,11 +10,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UsersRouteAcceptanceTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void retrieveEmptyUserListWithNoRegisteredUser() throws IOException, InterruptedException {
@@ -40,7 +46,7 @@ public class UsersRouteAcceptanceTest {
 
         HttpRequest request = HttpRequest.newBuilder()
             .POST(HttpRequest.BodyPublishers.ofString(
-                "{\"username\":\"pippo\", \"password\":\"pluto123\", \"about\":\"intro a pippo\"}"
+                "{\"username\":\"pippo\", \"password\":\"pluto123\", \"about\":\"About pippo user.\"}"
             ))
             .uri(URI.create("http://localhost:8000/users"))
             .build();
@@ -48,7 +54,14 @@ public class UsersRouteAcceptanceTest {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(201, response.statusCode());
-        assertThat(response.body()).contains("\"username\":\"pippo\"");
-        assertThat(response.body()).contains("\"about\":\"intro a pippo\"");
+        Map<String, Object> responseBody = stringJsonToMap(response.body());
+        assertEquals("pippo", responseBody.get("username"));
+        assertEquals("About pippo user.", responseBody.get("about"));
+        assertDoesNotThrow(() -> UUID.fromString((String) responseBody.get("id")));
+    }
+
+    private Map<String, Object> stringJsonToMap(String body) throws IOException {
+        TypeReference<HashMap<String, Object>> targetType = new TypeReference<>() { };
+        return objectMapper.readValue(body, targetType);
     }
 }
