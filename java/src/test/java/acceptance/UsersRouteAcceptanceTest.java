1630efbf3ecd52cd4f92c1e30061f582dd37937c
Nicola Moretto <moretto.nik@gmail.com>
2023-07-06 14:38:46 +0200

    Implemented register user test

--- java/src/test/java/acceptance/UsersRouteAcceptanceTest.java
+++ java/src/test/java/acceptance/UsersRouteAcceptanceTest.java
@@ -10,6 +10,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UsersRouteAcceptanceTest {
@@ -25,10 +26,28 @@ public class UsersRouteAcceptanceTest {
                 .uri(URI.create("http://localhost:8000/users"))
                 .build();
 
-
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         assertEquals(200, response.statusCode());
         assertEquals("[]", response.body());
+    }
+
+    @Disabled("WIP")
+    @Test
+    void registerUser() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString("{\"username\":\"pippo\", \"password\":\"pluto123\", \"about\":\"intro a pippo\"}"))
+                .uri(URI.create("http://localhost:8000/users"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(201, response.statusCode());
+        assertThat(response.body()).contains("\"username\":\"pippo\"");
+        assertThat(response.body()).contains("\"about\":\"intro a pippo\"");
     }
 }
