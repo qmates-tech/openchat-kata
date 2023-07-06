f8e18f4c1df13f5189174daef79b1a20b2e1fe01
Daniele Megna <megna.dany@gmail.com>
2023-07-06 11:06:29 +0200

    First disabled `/users` route acceptance test

---
+++ java/src/test/java/acceptance/UsersRouteAcceptanceTest.java
@@ -0,0 +1,35 @@
+package acceptance;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UsersRouteAcceptanceTest {
+
+    @Disabled("WIP")
+    @Test
+    void retrieveEmptyUserListWithNoRegisteredUser() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8000/users"))
+                .build();
+
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, response.statusCode());
+        assertEquals("[]", response.body());
+    }
+}
