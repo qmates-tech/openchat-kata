3e468d98b5d8f58fd5358c0d332a2e9a55fe9675
Daniele Megna <megna.dany@gmail.com>
2023-07-09 12:17:28 +0200

    Fake develop register user API
    user not stored yet

--- typescript/src/web/routes/users.ts
+++ typescript/src/web/routes/users.ts
@@ -1,12 +1,31 @@
 import { ServerResponse } from "http";
-import { jsonResponseWith, ParsedRequest } from "../router";
+import { jsonResponseWith, ParsedRequest, textResponse } from "../router";
+import * as uuid from 'uuid';
 
 function handle(request: ParsedRequest, response: ServerResponse): void {
-  jsonResponseWith([], 200, response)
+  switch (request.method) {
+    case 'GET':
+      jsonResponseWith([], 200, response)
+      return
+    case 'POST':
+      const username: String = request.requestBody.username
+      const userAbout: String = request.requestBody.about
+
+      jsonResponseWith({
+        "id": uuid.v4(),
+        "username": username,
+        "about": userAbout
+      }, 201, response)
+      return
+  }
+
+  textResponse(405, `Method ${request.method} not allowed!`, response)
+  return
 }
 
 function shouldHandle(r: ParsedRequest) {
-  return r.url === '/users' && r.method == 'GET'
+  return r.url === '/users'
+    && ['GET', 'POST'].includes(r.method)
 }
 
 export default {
--- typescript/tests/acceptance/UsersRouteAcceptance.spec.ts
+++ typescript/tests/acceptance/UsersRouteAcceptance.spec.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import * as uuid from 'uuid';
 
 describe('users API route', () => {
 
@@ -7,6 +8,20 @@ describe('users API route', () => {
     expect(response.status).toBe(200)
     expect(response.headers['content-type']).toBe("application/json")
     expect(response.data).toStrictEqual([])
+  })
+
+  test('register a user', async () => {
+    const response = await axios.post('http://localhost:8000/users', {
+      "username": "alice90",
+      "password": "pass1234",
+      "about": "About alice user."
+    })
+    expect(response.status).toBe(201)
+    expect(response.headers['content-type']).toBe("application/json")
+    const responseBody = response.data
+    expect(responseBody.username).toBe("alice90")
+    expect(responseBody.about).toBe("About alice user.")
+    expect(uuid.version(responseBody.id)).toBe(4)
   })
 
 })
