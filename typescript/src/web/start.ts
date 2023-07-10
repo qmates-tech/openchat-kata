d34eb2dd4ed589f14d8fe5bed25e0f1e64bf12f6
Daniele Megna <megna.dany@gmail.com>
2023-07-10 10:29:49 +0200

    Improve http server in start file
    - better separate request.on body read phase with
      parse & process phase extracting parseAndProcessRequest function
    - properly catch errors in request handling, they weren't caught

--- typescript/src/web/start.ts
+++ typescript/src/web/start.ts
@@ -2,31 +2,32 @@ import http, { IncomingMessage, ServerResponse } from 'http';
 import { emptyResponse, handleReceivedRequest, ParsedRequest } from './router';
 
 console.log("Starting the server at localhost:8000 ...");
-
 http.createServer((request: IncomingMessage, response: ServerResponse) => {
-  try {
-    const { url, method } = request
-    console.log(`Received ${method} on ${url}`)
+  let receivedData = '';
+  request.on('data', chunk => receivedData += chunk);
+  request.on('end', () => parseAndProcessRequest(request, receivedData, response));
+}).listen(8000)
 
-    let receivedData = '';
-    request.on('data', chunk => receivedData += chunk);
-    request.on('end', () => {
-      const requestBody = parseRequestBody(receivedData)
-      const parsedRequest: ParsedRequest = {
-        method: method!,
-        url: url!,
-        requestBody: requestBody
-      }
-      handleReceivedRequest(parsedRequest, response)
-    });
-  } catch (error) {
+function parseAndProcessRequest(request: IncomingMessage, receivedData: string, response: ServerResponse) {
+  const { url, method } = request
+  console.log(`Received ${method} on ${url}`)
+
+  try {
+    const requestBody = parseRequestBody(receivedData)
+    const parsedRequest: ParsedRequest = {
+      method: method!,
+      url: url!,
+      requestBody: requestBody
+    }
+    handleReceivedRequest(parsedRequest, response)
+  } catch(error) {
     console.log('Error during request handling!', error)
     emptyResponse(500, response)
   }
-}).listen(8000);
+}
 
 function parseRequestBody(receivedData: string): any {
-  if(!receivedData || receivedData == '') {
+  if (!receivedData || receivedData == '') {
     console.log('Empty request body')
     return undefined
   }
@@ -35,7 +36,7 @@ function parseRequestBody(receivedData: string): any {
     const requestBody: any = JSON.parse(receivedData)
     console.log(`Received request body: ${JSON.stringify(requestBody)}`)
     return requestBody
-  } catch(error) {
+  } catch (error) {
     console.log('Invalid not-empty request body', receivedData)
     return undefined
   }
