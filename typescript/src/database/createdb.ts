1d84b3362ba7c7a5b8ec85444dcdd92d77a3e2e8
Daniele Megna <megna.dany@gmail.com>
2023-07-12 15:40:48 +0200

    Improve createdb TS script with console logs

--- typescript/src/database/createdb.ts
+++ typescript/src/database/createdb.ts
@@ -1,14 +1,17 @@
-import fs from 'fs';
 import Database from 'better-sqlite3';
+import fs from 'fs';
 
 if (process.argv.length === 2) {
-    console.error('ERROR! Usage: node createdb.js <filepath>');
-    process.exit(1);
-  }
+  console.error('ERROR! Usage: node createdb.js <filepath>')
+  process.exit(1)
+}
 
 const dbFilename = process.argv[2]
-const db = new Database(dbFilename)
+console.log('Creating database file ' + dbFilename + ' ...')
 
+const db = new Database(dbFilename)
 const dbSchemaFilename = 'src/database/schema.sql'
-const dbSchemaSql = fs.readFileSync(dbSchemaFilename, 'utf8');
-db.exec(dbSchemaSql);
+const dbSchemaSql = fs.readFileSync(dbSchemaFilename, 'utf8')
+db.exec(dbSchemaSql)
+
+console.log('Succesfully created!')
