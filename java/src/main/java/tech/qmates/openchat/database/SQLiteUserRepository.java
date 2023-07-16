da11981174c634db262553813f804bb5c6666b15
Daniele Megna <megna.dany@gmail.com>
2023-07-16 17:30:02 +0200

    Developing SQLiteUserRepository with integration tests

--- java/src/main/java/tech/qmates/openchat/database/SQLiteUserRepository.java
+++ java/src/main/java/tech/qmates/openchat/database/SQLiteUserRepository.java
@@ -4,11 +4,10 @@ import tech.qmates.openchat.domain.entity.RegisteredUser;
 import tech.qmates.openchat.domain.entity.UserToRegister;
 import tech.qmates.openchat.domain.repository.UserRepository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class SQLiteUserRepository implements UserRepository {
 
@@ -20,11 +19,17 @@ public class SQLiteUserRepository implements UserRepository {
 
     @Override
     public void store(UserToRegister user) {
-        try {
-            Connection connection = openConnection();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-            connection.close();
+        try(Connection connection = openConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO users(id, username, password, about) VALUES (?,?,?,?)"
+            );
+
+            statement.setString(1, user.uuid().toString());
+            statement.setString(2, user.username());
+            statement.setString(3, user.password());
+            statement.setString(4, user.about());
+
+            statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -37,12 +42,31 @@ public class SQLiteUserRepository implements UserRepository {
 
     @Override
     public List<RegisteredUser> getAll() {
-        return null;
+        try(Connection connection = openConnection()) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+            List<RegisteredUser> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(new RegisteredUser(
+                    UUID.fromString(rs.getString("id")),
+                    rs.getString("username"),
+                    rs.getString("about")
+                ));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void reset() {
-
+        try(Connection connection = openConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("DELETE FROM users");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Connection openConnection() throws SQLException {
--- java/src/test/java/integration/tech/qmates/openchat/database/SQLiteUserRepositoryTest.java
+++ java/src/test/java/integration/tech/qmates/openchat/database/SQLiteUserRepositoryTest.java
@@ -2,17 +2,61 @@ package integration.tech.qmates.openchat.database;
 
 import org.junit.jupiter.api.Test;
 import tech.qmates.openchat.database.SQLiteUserRepository;
+import tech.qmates.openchat.domain.entity.RegisteredUser;
 import tech.qmates.openchat.domain.entity.UserToRegister;
 import tech.qmates.openchat.domain.repository.UserRepository;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SQLiteUserRepositoryTest {
 
-    private static final String SQLITE_FILEPATH = "src/test/integration.test.db";
-    private final UserRepository repository = new SQLiteUserRepository(SQLITE_FILEPATH);
+    UserRepository repository = buildRepository();
 
     @Test
-    void fooo() {
-        UserToRegister userToStore = UserToRegister.newWith("alice90", "pass1234", "About alice user.");
-        repository.store(userToStore);
+    void getUsersFromEmptyRepository() {
+        List<RegisteredUser> users = repository.getAll();
+
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    void fillGetAllAndResetRepository() {
+        UserToRegister alice = UserToRegister.newWith("alice90", "pass1234", "About alice user.");
+        UserToRegister bob = UserToRegister.newWith("bob89", "123pass", "About bob user.");
+        repository.store(alice);
+        repository.store(bob);
+
+        List<RegisteredUser> users = repository.getAll();
+        assertEquals(2, users.size());
+        assertThat(users).anySatisfy(user -> {
+            assertEquals(alice.uuid(), user.uuid());
+            assertEquals("alice90", alice.username());
+            assertEquals("About alice user.", alice.about());
+        });
+        assertThat(users).anySatisfy(user -> {
+            assertEquals(bob.uuid(), user.uuid());
+            assertEquals("bob89", bob.username());
+            assertEquals("About bob user.", bob.about());
+        });
+
+        repository.reset();
+        assertEquals(0, repository.getAll().size());
+    }
+
+    private UserRepository buildRepository() {
+        URL res = getClass().getClassLoader().getResource("integration.test.db");
+        if (res == null)
+            throw new RuntimeException("Cannot find integration test database file!");
+
+        try {
+            return new SQLiteUserRepository(res.toURI().getPath());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
