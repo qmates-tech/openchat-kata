cf77749ede566237eee88a5aab642f435a142211
Daniele Megna <megna.dany@gmail.com>
2023-07-18 18:57:09 +0200

    Print all request attributes when thrownException has not been found !

--- java/src/main/java/tech/qmates/openchat/web/Main.java
+++ java/src/main/java/tech/qmates/openchat/web/Main.java
@@ -10,6 +10,7 @@ import tech.qmates.openchat.web.servlets.AdminServlet;
 import tech.qmates.openchat.web.servlets.UsersServlet;
 
 import java.util.Arrays;
+import java.util.Enumeration;
 
 public class Main {
 
@@ -33,14 +34,17 @@ public class Main {
         @Override
         public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) {
             System.out.println("Unhandled thrownException occurred!");
-            Throwable thrownException = getThrownException(request);
-            if (thrownException != null)
+            Throwable thrownException = findThrownException(request);
+            if (thrownException != null) {
                 thrownException.printStackTrace();
+            } else {
+                printAllAttributes(request);
+            }
 
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
 
-        private static Throwable getThrownException(HttpServletRequest request) {
+        private static Throwable findThrownException(HttpServletRequest request) {
             for (String attribute : Arrays.asList(
                 "jakarta.servlet.error.thrownException",
                 "jakarta.servlet.error.exception"
@@ -52,5 +56,14 @@ public class Main {
 
             return null;
         }
+
+        private void printAllAttributes(HttpServletRequest request) {
+            Enumeration<String> attributeNames = request.getAttributeNames();
+            while (attributeNames.hasMoreElements()) {
+                String attributeName = attributeNames.nextElement();
+                System.out.println(attributeName + " = " + request.getAttribute(attributeName));
+            }
+        }
+
     }
 }
