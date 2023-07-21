856aefc01ed3e0607a94813ed57201fffd07f31f
Daniele Megna <megna.dany@gmail.com>
2023-07-21 17:59:51 +0200

    Minor improves in TimelineRouteAcceptanceTest

--- java/src/test/java/acceptance/tech/qmates/openchat/web/TimelineRouteAcceptanceTest.java
+++ java/src/test/java/acceptance/tech/qmates/openchat/web/TimelineRouteAcceptanceTest.java
@@ -15,18 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TimelineRouteAcceptanceTest extends BaseOpenChatRouteAcceptanceTest {
 
     @Test
-    void unexistingUserTimeline() throws IOException, InterruptedException {
-        UUID unexistingUUID = UUID.randomUUID();
-        HttpRequest request = requestBuilderFor("/users/" + unexistingUUID + "/timeline").GET().build();
-
-        HttpResponse<String> response = send(request);
-
-        assertEquals(404, response.statusCode());
-        assertEquals("text/plain;charset=utf-8", response.headers().firstValue("Content-Type").get());
-        assertEquals("User not found.", response.body());
-    }
-
-    @Test
     void emptyUserTimeline() throws IOException, InterruptedException {
         String existingUserId = registerAUser();
         HttpRequest request = requestBuilderFor("/users/" + existingUserId + "/timeline").GET().build();
@@ -62,8 +50,9 @@ public class TimelineRouteAcceptanceTest extends BaseOpenChatRouteAcceptanceTest
     }
 
     @Test
-    void timelineForInvalidUserId() throws IOException, InterruptedException {
-        HttpRequest request = requestBuilderFor("/users/invalid/timeline").GET().build();
+    void unexistingUserTimeline() throws IOException, InterruptedException {
+        UUID unexistingUUID = UUID.randomUUID();
+        HttpRequest request = requestBuilderFor("/users/" + unexistingUUID + "/timeline").GET().build();
 
         HttpResponse<String> response = send(request);
 
@@ -72,5 +61,15 @@ public class TimelineRouteAcceptanceTest extends BaseOpenChatRouteAcceptanceTest
         assertEquals("User not found.", response.body());
     }
 
+    @Test
+    void invalidUserIdTimeline() throws IOException, InterruptedException {
+        HttpRequest request = requestBuilderFor("/users/invalid/timeline").GET().build();
+
+        HttpResponse<String> response = send(request);
+
+        assertEquals(404, response.statusCode());
+        assertEquals("text/plain;charset=utf-8", response.headers().firstValue("Content-Type").get());
+        assertEquals("User not found.", response.body());
+    }
 
 }
